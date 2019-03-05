4d03bb6588ebf10a24ebd6283482818e5cb9b5a7
marcais <jean.marcais@gmail.com>
2019-03-05 23:21:23 +0100

    Update threshold_function2.m
    Update now threshold_function2.m is identical to threshold_function.m

--- src_matlab/tools/boussinesq_resolution_tools/threshold_function2.m
+++ src_matlab/tools/boussinesq_resolution_tools/threshold_function2.m
@@ -1,10 +1,15 @@
 function OUT=threshold_function2(x)
 % sigmoid function centered on 1 so it is concave
-    OUT=1-1./(1+exp(-10000*(x-0.995)));
+% %     OUT=1-1./(1+exp(-100*(x-0.99)));
 %     OUT=1-2./(1+exp(-1000*(x-1)));
-%     OUT=1-exp(-100000*(1-x));
+    OUT=1-exp(-1000*(1-x));
 %     OUT=double(x>1)+(x<=1).*(1-exp(-1000*(1-x)));
-%     OUT=1-exp(-1000*(1-x));
+% % % % %     OUT=1-1.01./(1+exp(-((x-1))/1e-2+log(0.01)));%2./(1+exp(-(x-1)/1e-3));
+% % % % % % % % % %     K=1.0001;
+% % % % % % % % % %     nu=0.01;%100;%5;
+% % % % % % % % % %     B=100;%1000;
+% % % % % % % % % %     OUT=1-K./(1+(K^nu-1)*exp(-B*(x-1))).^(1/nu);
+%     OUT=1-exp(-100*(1-x));
 %     OUT=1*ones(size(x));
 %     OUT=1-1./(1+exp(-1000*(x-0.99)));   
 % OUT=x<1;
