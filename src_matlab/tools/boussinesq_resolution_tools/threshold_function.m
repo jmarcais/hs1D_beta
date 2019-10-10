0358241d0d4849a910eb558dd257e33b75eee2c1
jmarcais <45568980+jmarcais@users.noreply.github.com>
2019-10-10 10:42:23 +0200

    Update threshold_function.m
    Minor changes

--- src_matlab/tools/boussinesq_resolution_tools/threshold_function.m
+++ src_matlab/tools/boussinesq_resolution_tools/threshold_function.m
@@ -4,7 +4,7 @@ function OUT=threshold_function(x)
 %     OUT=1-2./(1+exp(-1000*(x-1)));
 %     OUT=1-exp(-100000*(1-x));
 %     OUT=double(x>1)+(x<=1).*(1-exp(-1000*(1-x)));
-    OUT=1-exp(-1000*(1-x));
+    OUT=1-exp(-10*(1-x));
 %     OUT=1*ones(size(x));
 %     OUT=1-1./(1+exp(-1000*(x-0.99)));   
 % OUT=x<1;
